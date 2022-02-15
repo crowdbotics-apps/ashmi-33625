4ade84be9bf3e52c401bc19ea647c547c0aff23b
Crowdbotics <team@crowdbotics.com>
2022-02-15 10:56:02 +0000

    Initial commit.

---
+++ backend/ashmi_33625/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ashmi_33625 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ashmi_33625.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ashmi_33625",
+  url: "https://ashmi_33625.botics.co",
+  api: "https://ashmi_33625.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
